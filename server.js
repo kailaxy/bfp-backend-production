609f9f9d9e5d3a04141b9ac56de784a4fac20ee1
kailaxy <kylesermon10@gmail.com>
2025-10-08 20:22:07 +0800

    Fix CORS: Add PATCH method to allowed methods for password reset

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const corsOptions = {
     /\.onrender\.com$/
   ],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
 
